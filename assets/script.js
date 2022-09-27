7adfb7b1cf5834d8080898aaebb378cd50b18c8e
Michael Crandall <mec192@gmail.com>
2022-09-26 22:07:21 -0500

    added background and third api

--- assets/script.js
+++ assets/script.js
@@ -12,10 +12,25 @@ console.log("hello me")
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
 
-fetch("http://api.openweathermap.org/geo/1.0/zip?zip=90210,US&appid=2d8868d62bc83f43b64634172d198128")
-.then(function(response) {
-	return response.json();
-})
-.then(function(data){
-	console.log(data);
-})
+// fetch("http://api.openweathermap.org/geo/1.0/zip?zip=90210,US&appid=2d8868d62bc83f43b64634172d198128")
+// .then(function(response) {
+// 	return response.json();
+// })
+// .then(function(data){
+// 	console.log(data);
+// })
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 		'X-RapidAPI-Key': 'fbec7f460cmsh28145391e03528fp1b45d3jsn9d08ce2c7a7e',
+// 		'X-RapidAPI-Host': 'distance-calculator.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://distance-calculator.p.rapidapi.com/distance/simple?lat_1=47.373535&long_1=8.541109&lat_2=42.335321&long_2=-71.023516&unit=miles&decimal_places=2', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
