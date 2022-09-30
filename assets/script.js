9c7669b80778f4d2ecdfbfa5746ab226de8aea95
Michael Crandall <mec192@gmail.com>
2022-09-29 19:42:41 -0500

    added append java

--- assets/script.js
+++ assets/script.js
@@ -51,27 +51,37 @@ function distanceCalc(L1, Lo1, L2, Lo2, km) {
     `https://distance-calculator.p.rapidapi.com/distance/simple?lat_1=${L1}&long_1=${Lo1}&lat_2=${L2}&long_2=${Lo2}&unit=miles&decimal_places=2`,
     options
   )
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-      var km = response.distance / 1.609;
+    // .then((response) => response.json())
+    // .then((response) => {
+    //   console.log(response);
+    //   var km = response.distance / 1.609;
 
-      const optionsCarbon = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "b8c6fb145fmsh124aed16ff0541dp1b6734jsn653a01a171f0",
-          "X-RapidAPI-Host": "carbonfootprint1.p.rapidapi.com",
-        },
-      };
+    //   const optionsCarbon = {
+    //     method: "GET",
+    //     headers: {
+    //       "X-RapidAPI-Key":
+    //         "b8c6fb145fmsh124aed16ff0541dp1b6734jsn653a01a171f0",
+    //       "X-RapidAPI-Host": "carbonfootprint1.p.rapidapi.com",
+    //     },
+    //   };
 
     //   fetch(
     //     `https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel?distance=${km}&vehicle=MediumPetrolCar`,
     //     optionsCarbon
     //   )
     //     .then((response) => response.json())
-    //     .then((response) => console.log(response))
-    //     .catch((err) => console.error(err));
+    //     .then((response) => {
+    //         console.log(response)
+    //         var carbonEquivalent = response.carbonEquivalent
+    //     $(".finalAnswer").append("<h1>" + carbonEquivalent + "   CO2 in Kg</h1>")
+        
+    //     })
+        
+        
+        
     // })
-    // .catch((err) => console.error(err));
-// }
+
+    .catch((err) => console.error(err));
+
+
+};
